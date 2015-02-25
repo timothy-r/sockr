2c8ffeb5dcb5c20f44f5fef15a84d38068ed788a
Tim Rodger <tim.rodger@gmail.com>
2015-02-25 00:27:52 +0000

    connect and disconnect socket on server

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  console.log('responding to /');
+  console.log('request for /');
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, function(){
